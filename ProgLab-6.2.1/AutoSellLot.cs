f215a3edcbcd984ade0c96c4677c83ec05bc44b0
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2020-06-05 12:40:41 +0300

    Add files via upload

---
+++ ProgLab-6.2.1/AutoSellLot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgLab_6._2._1
+{
+    class AutoSellLot
+    {
+    }
+}
---
+++ ProgLab-6.2.1/Lot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgLab_6._2._1
+{
+    class Lot
+    {
+        public enum LotTypes
+        {
+            Auction,
+            QuickSelling,
+            Bidding
+        }
+        public int MinPrice { get; private set; }
+        public DateTime ExpireDate { get; private set; } = DateTime.Today.AddDays(7);
+        public string Description { get; private set; } = "Standart shipping";
+        public Lot(int minPrice, DateTime expireDate, string description)
+        {
+            MinPrice = minPrice;
+            ExpireDate = expireDate;
+            Description = description;
+        }
+    }
+}
